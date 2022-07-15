abd8302afe66354c20160d4826fa1f6c6f77dd46
Dimitri <dimitri.trigaloux@ekimetrics.com>
2022-07-15 11:57:04 +0200

    test

--- ex02/linear_interpolation.py
+++ ex02/linear_interpolation.py
@@ -1,8 +1,8 @@
+from Matrix import Matrix
+from Vector import Vector
 import sys
 sys.path.append('.')
 
-from Vector import Vector
-from Matrix import Matrix
 
 def linear_interpolation(o1, o2, coef):
     if not isinstance(coef, (int, float)):
@@ -30,19 +30,23 @@ def linear_interpolation(o1, o2, coef):
 
 if __name__ == '__main__':
 
-    print('\033[7;49;32mNumber\033[0m\n')
+    print('\033[7;49;32mNumber \033[0m\n')
     linear_interpolation(1, 5, 0.25)
     linear_interpolation(-1, 9, 0.5)
     linear_interpolation(1, 11, 0.75)
     linear_interpolation(1, 11, 1)
 
-    print('\n\033[7;49;32mVector\033[0m\n')
+    print('\n\033[7;49;32mVector \033[0m\n')
     linear_interpolation(Vector([1, 2, 3]), Vector([2, 5, 5]), 0)
     linear_interpolation(Vector([1, 2, 3]), Vector([2, 5, 5]), 1)
     linear_interpolation(Vector([1, 2, 3]), Vector([2, 5, 5]), 0.5)
 
-    print('\033[7;49;32mMatrix\033[0m\n')
-    linear_interpolation(Matrix([[1, 2, 3], [1, 4, 3], [1, 2, 6]]), Matrix([[1, 2, 3], [2, 5, 5], [10, 4, 10]]), 0)
-    linear_interpolation(Matrix([[1, 2, 3], [1, 4, 3], [1, 2, 6]]), Matrix([[1, 2, 3], [2, 5, 5], [10, 4, 10]]), 0.5)
-    linear_interpolation(Matrix([[1, 2, 3], [1, 4, 3], [1, 2, 6]]), Matrix([[1, 2, 3], [2, 5, 5], [10, 4, 10]]), 1)
-    linear_interpolation(Matrix([[1, 1, 1], [1, 1, 1], [1, 1, 1]]), Matrix([[11, 11, 11], [101, 101, 101], [4, 5, 6]]), 0.4)
+    print('\033[7;49;32mMatrix \033[0m\n')
+    linear_interpolation(Matrix([[1, 2, 3], [1, 4, 3], [1, 2, 6]]), Matrix(
+        [[1, 2, 3], [2, 5, 5], [10, 4, 10]]), 0)
+    linear_interpolation(Matrix([[1, 2, 3], [1, 4, 3], [1, 2, 6]]), Matrix(
+        [[1, 2, 3], [2, 5, 5], [10, 4, 10]]), 0.5)
+    linear_interpolation(Matrix([[1, 2, 3], [1, 4, 3], [1, 2, 6]]), Matrix(
+        [[1, 2, 3], [2, 5, 5], [10, 4, 10]]), 1)
+    linear_interpolation(Matrix([[1, 1, 1], [1, 1, 1], [1, 1, 1]]), Matrix(
+        [[11, 11, 11], [101, 101, 101], [4, 5, 6]]), 0.4)
